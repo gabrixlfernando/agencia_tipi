d5cacebdc8e5c2e3b837c2428c6f6d340fbfe9ac
gabrixlfernando <biel_nando2012@hotmail.com>
2023-08-03 13:31:03 -0300

    .

--- conteudo/depoimento.php
+++ conteudo/depoimento.php
@@ -1,56 +1,56 @@
 <section data-aos="fade-up" data-aos-duration="900" class="depor">
-        <div class="site">
-          <h2>Depoimentos</h2>
-          <div class="depoimento">
-            <img data-aos="zoom-in" data-aos-duration="900" src="img/imgDepoimento.png" alt="Depoimento" />
+  <div class="site">
+    <h2>Depoimentos</h2>
+    <div class="depoimento">
+      <img data-aos="zoom-in" data-aos-duration="900" src="img/imgDepoimento.png" alt="Depoimento" />
 
-            <div class="deposlide">
-              <div>
-                <img src="img/pessoas.png" alt="User Depoimento" />
+      <div class="deposlide">
+        <div>
+          <img src="img/pessoas.png" alt="User Depoimento" />
 
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Reprehenderit ab velit placeat corporis error illo deleniti.
-                  Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
-                  tempore est eveniet nisi quas accusamus!
-                </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit ab velit placeat corporis error illo deleniti.
+            Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
+            tempore est eveniet nisi quas accusamus!
+          </p>
 
-                <div class="nomedata">
-                  <h2>Nome</h2>
-                  <h2>Data</h2>
-                </div>
-              </div>
-              <div>
-                <img src="img/pessoas.png" alt="User Depoimento" />
-
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Reprehenderit ab velit placeat corporis error illo deleniti.
-                  Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
-                  tempore est eveniet nisi quas accusamus!
-                </p>
-
-                <div class="nomedata">
-                  <h2>Nome</h2>
-                  <h2>Data</h2>
-                </div>
-              </div>
-              <div>
-                <img src="img/pessoas.png" alt="User Depoimento" />
-
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Reprehenderit ab velit placeat corporis error illo deleniti.
-                  Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
-                  tempore est eveniet nisi quas accusamus!
-                </p>
-
-                <div class="nomedata">
-                  <h2>Nome</h2>
-                  <h2>Data</h2>
-                </div>
-              </div>
-            </div>
+          <div class="nomedata">
+            <h2>Nome</h2>
+            <h2>Data</h2>
           </div>
         </div>
-      </section>
+        <div>
+          <img src="img/pessoas.png" alt="User Depoimento" />
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit ab velit placeat corporis error illo deleniti.
+            Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
+            tempore est eveniet nisi quas accusamus!
+          </p>
+
+          <div class="nomedata">
+            <h2>Nome</h2>
+            <h2>Data</h2>
+          </div>
+        </div>
+        <div>
+          <img src="img/pessoas.png" alt="User Depoimento" />
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit ab velit placeat corporis error illo deleniti.
+            Quia ullam, adipisci obcaecati cum sequi corrupti nemo,
+            tempore est eveniet nisi quas accusamus!
+          </p>
+
+          <div class="nomedata">
+            <h2>Nome</h2>
+            <h2>Data</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
